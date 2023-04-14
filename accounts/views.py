843028cb1fe2a83cfb2f9b8d558e5d27466fdca1
tichvu2611 <tichvu2611@gmail.com>
2023-04-14 08:11:07 -0400

    fix register user

--- accounts/views.py
+++ accounts/views.py
@@ -12,5 +12,5 @@ from accounts.models import CustomUserCreationForm
 
 class SignupView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('account:login')
     template_name = 'account/signup.html'
